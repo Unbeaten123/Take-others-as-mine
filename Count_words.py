0940c63ba6efd777823c6f6f2be433d4efc6ff97
Unbeaten123 <yuzhizhou123@gmail.com>
2016-03-04 23:43:03 +0800

    Count 10 most common words

--- Count_words.py
+++ Count_words.py
@@ -4,13 +4,14 @@ from collections import Counter
 
 textpath = raw_input("Enter the path of a text file: ")
 
-'''函数：输入一个英文的纯文本文件，统计其中单词个数'''
+#函数：输入一个英文的纯文本文件，统计其中单词个数
 def getMostCommonWord(articlefilesource):
-    pattern = '''[A-Za-z]+'''
+    #模式代表匹配普通单词，包含’s形式的如one's
+    pattern = '''[a-zA-Z]+\'?[a-zA-z]+'''
     f = open(articlefilesource)
     r = re.findall(pattern,f.read())
-    '''只返回10个频度最高的单词'''
-    return Counter(r).most_common(10)
+    #只返回10个频度最高的单词
+    return Counter(r).most_common()
 
 wordtuple = getMostCommonWord(textpath)
 
